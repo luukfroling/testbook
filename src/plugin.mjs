13238ec1c8a31c3fc9799b915150f233f32a0757
luukfroling <L.Froling@student.tudelft.nl>
2025-01-31 00:25:39 +0100

    update

--- src/plugin.mjs
+++ src/plugin.mjs
@@ -1,6 +1,21 @@
+const myDirective = {
+  name: "mydirective",
+  doc: "My new directive!",
+  arg: { type: String, doc: "The word to display" },
+  run(data, vfile, ctx) {
+    console.log("Running my directive with data:", data);
+    const word = data.arg
+    const sentence = {
+      type: "text",
+      value: "The word you gave is: " + word,
+    };
+    return [{type: "paragraph", children: [sentence]}];
+  },
+};
+
 const plugin = {
     name: "My cool plugin",
-    directives: [],
+    directives: [myDirective],
   };
   
 export default plugin;
