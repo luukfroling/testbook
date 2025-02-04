e08b29f84f0af36ea05c32fc441114da5376cbdc
luukfroling <L.Froling@student.tudelft.nl>
2025-02-04 02:40:31 +0100

    das

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
 //find and replace "loading likes ..." with "Likes: 0"
-
-document.body.addEventListener("change", function() {
-    let stringToFind = "Loading...";
-    let stringToReplace = "Likes: 0";
-    
-    //Find string in current page
-    let body = document.body.innerHTML;
-    let bodyNew = body.replace(stringToFind, stringToReplace);
-    document.body.innerHTML = bodyNew;
-    
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.addEventListener("change", function() {
+        let stringToFind = "Loading...";
+        let stringToReplace = "Likes: 0";
+        
+        //Find string in current page
+        let body = document.body.innerHTML;
+        let bodyNew = body.replace(stringToFind, stringToReplace);
+        document.body.innerHTML = bodyNew;
+        
+    });    
 });
 
 
