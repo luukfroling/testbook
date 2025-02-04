907a0241156983f2e83f335940438e719be6e9ee
luukfroling <L.Froling@student.tudelft.nl>
2025-02-04 02:59:27 +0100

    body =

--- assets/script.js
+++ assets/script.js
@@ -12,18 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
 let loadItem = (body) => {
     body = document.body.innerHTML;
     let stringToFind = "Loading...";
-    let intermediateString = "Loading.."
+    let intermediateString = "Loading .."
     let stringToReplace = "Likes: 0";
     
     //Find string in current page
     if(body.includes(stringToFind)){
-        body.replace(stringToFind, intermediateString);
+        body = body.replace(stringToFind, intermediateString);
         document.body.innerHTML = body;
         console.log("Loading likes ... found");
         return setTimeout(loadItem, 1000);
     } else if(body.includes(intermediateString)){
         console.log("Loading likes ... intermediate found");
-        body.replace(intermediateString, stringToReplace);
+        body = body.replace(intermediateString, stringToReplace);
         document.body.innerHTML = body;
         return setTimeout(loadItem, 1000);
     } else {
