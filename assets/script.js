e05a3a3d7f09c5d7e24bedadafdb904c64f8836a
luukfroling <L.Froling@student.tudelft.nl>
2025-02-04 05:25:52 +0100

    lafn

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,19 @@
-//find and replace "loading likes ..." with "Likes: 0"
-document.addEventListener("DOMContentLoaded", function() {
-    
-    // set 1 second delay and keep repeating
-    let body;
-    loadItem(body)
+/*
+* 03/02/2025, Luuk Fröling
+*/
 
-        
-  
+document.addEventListener("DOMContentLoaded", function() {
+    let body;
+    loadItem(body)  
+    getLikes(body);
 });
 
-let loadItem = (body) => {
+
+/* because jupyter book next seems to reload the page after loading it the first time,
+*   we check again after a second to see if our data is displayed properly.  The strings can be changed, then also change string in md file.
+*/
+
+let loadItem = (body, text) => {
     body = document.body.innerHTML;
     let stringToFind = "Loading...";
     let intermediateString = "Loading.. "
@@ -17,19 +21,25 @@ let loadItem = (body) => {
     
     //Find string in current page
     if(body.includes(stringToFind)){
-        body = body.replace(stringToFind, intermediateString);
-        document.body.innerHTML = body;
-        console.log("Loading likes ... found");
+        document.body.innerHTML = body.replace(stringToFind, intermediateString);
         return setTimeout(loadItem, 1000);
     } else if(body.includes(intermediateString)){
-        console.log("Loading likes ... intermediate found");
-        body = body.replace(intermediateString, stringToReplace);
-        document.body.innerHTML = body;
+        document.body.innerHTML = body.replace(intermediateString, stringToReplace);;
         return setTimeout(loadItem, 1000);
     } else {
-        console.log("Loading likes ... not found");
         return; 
     }
+}
+
+/* Load data from github json file
+*
+*/
+
+let getLikes = (body) => {
+    fetch("https://raw.githubusercontent.com/luukfroling/testbook/main/data/data.json")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error loading JSON:", error));
 }
 
 
