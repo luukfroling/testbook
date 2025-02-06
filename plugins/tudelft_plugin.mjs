1c4dff6a706a0022e123136e052c77d96250bf4e
luukfroling <L.Froling@student.tudelft.nl>
2025-02-06 09:55:20 +0100

    asd

--- plugins/tudelft_plugin.mjs
+++ plugins/tudelft_plugin.mjs
@@ -5,21 +5,39 @@ const plugin = {
         name: 'transform-typography',
         doc: 'An example transform that rewrites bold text as text with emphasis.',
         stage: 'document',
-        plugin: (_, utils) => (node) => {
-            console.log("one below root: "); 
-            console.log(utils.select('root', node).children[0]);
-            console.log("two below root (one below that): ");
-            console.log(utils.select('root', node).children[0].children[0]);
-
-          //find root and set all text color to red
-        //  utils.select('root', node).children.forEach((child) => {
-        //      if (child.type === 'text') {
-        //          child.value = `<span style="color: red;">${child.value}</span>`;
-            //     }
-            // });
+        transform: (tree, vfile) => {
+            console.log(tree, vfile);
         },
       },
     ],
   };
   
   export default plugin;
+
+
+
+// const plugin = {
+//     name: 'Strong to emphasis',
+//     transforms: [
+//       {
+//         name: 'transform-typography',
+//         doc: 'An example transform that rewrites bold text as text with emphasis.',
+//         stage: 'document',
+//         plugin: (_, utils) => (node) => {
+//             console.log("one below root: "); 
+//             console.log(utils.select('root', node).children[0]);
+//             console.log("two below root (one below that): ");
+//             console.log(utils.select('root', node).children[0].children[0]);
+
+//           //find root and set all text color to red
+//         //  utils.select('root', node).children.forEach((child) => {
+//         //      if (child.type === 'text') {
+//         //          child.value = `<span style="color: red;">${child.value}</span>`;
+//             //     }
+//             // });
+//         },
+//       },
+//     ],
+//   };
+  
+//   export default plugin;
