6819807c50a1da85d932c677c7a691e269bbfbda
luukfroling <L.Froling@student.tudelft.nl>
2025-02-06 09:38:28 +0100

    asd

--- plugins/tudelft_plugin.mjs
+++ plugins/tudelft_plugin.mjs
@@ -6,7 +6,13 @@ const plugin = {
         doc: 'An example transform that rewrites bold text as text with emphasis.',
         stage: 'document',
         plugin: (_, utils) => (node) => {
-          console.log(utils.select('root', node));
+          console.log(utils.select('root', node).value);
+          //find root and set all text color to red
+            // utils.select('root', node).children.forEach((child) => {
+            //     if (child.type === 'text') {
+            //     child.value = `<span style="color: red;">${child.value}</span>`;
+            //     }
+            // });
         },
       },
     ],
