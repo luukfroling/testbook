465f1ca4ffb30ea5570e0eb133b52096a95236ad
luukfroling <L.Froling@student.tudelft.nl>
2025-02-06 09:15:37 +0100

    update

--- plugins/tudelft_plugin.mjs
+++ plugins/tudelft_plugin.mjs
@@ -1,8 +1,15 @@
-export const tudelftPlugin = {
+const tudelftPlugin = {
     name: "tudelft-style-plugin",
     transform: (tree, vfile) => {
       vfile.data.frontmatter = vfile.data.frontmatter || {};
       vfile.data.frontmatter.extra_head = vfile.data.frontmatter.extra_head || [];
       vfile.data.frontmatter.extra_head.push('<link rel="stylesheet" href="/assets/tudelft_style.css">');
-    }
+    },
   };
+  
+  const plugin = {
+    name: "TU Delft Style Plugin",
+    transforms: [tudelftPlugin],
+  };
+  
+  export default plugin;
